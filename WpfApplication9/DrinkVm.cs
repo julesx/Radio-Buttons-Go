fc54791e52e0523ce30848620970dbaca1a1f65f
Julien Rosen <jlrosen416@gmail.com>
2016-08-18 11:54:29 -0400

    Add project files.

---
+++ WpfApplication9/DrinkVm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace WpfApplication9
+{
+    [ImplementPropertyChanged]
+    public class DrinkVm
+    {
+        public Drink Drink { get; set; }
+        public bool IsChecked { get; set; }
+
+        public DrinkVm(Drink drink, bool isChecked = false)
+        {
+            Drink = drink;
+            IsChecked = isChecked;
+        }
+    }
+}
---
+++ WpfApplication9/MainWindowVm.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using PropertyChanged;
+
+namespace WpfApplication9
+{
+    public enum Drink { Coke, Pepsi, MountainDew }
+
+    [ImplementPropertyChanged]
+    public class MainWindowVm
+    {
+        private RelayCommand _cmdCheckCommand;
+        public ICommand CmdCheckCommand => _cmdCheckCommand ?? (_cmdCheckCommand = new RelayCommand(CheckCommand));
+
+        private void CheckCommand(object obj)
+        {
+            var drinkVm = (DrinkVm)obj;
+
+            foreach (var drink in Drinks)
+                drink.IsChecked = false;
+
+            drinkVm.IsChecked = true;
+            SelectedDrink = drinkVm;
+        }
+
+        public ObservableCollection<DrinkVm> Drinks { get; set; } = new ObservableCollection<DrinkVm>();
+
+        public DrinkVm SelectedDrink { get; set; }
+
+        public MainWindowVm()
+        {
+            Drinks.Add(new DrinkVm(Drink.Coke, true));
+            Drinks.Add(new DrinkVm(Drink.Pepsi));
+            Drinks.Add(new DrinkVm(Drink.MountainDew));
+
+            SelectedDrink = Drinks.First();
+        }
+    }
+}
